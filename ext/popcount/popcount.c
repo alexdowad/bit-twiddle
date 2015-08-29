c8a3466475cdb2125496049653432ee6680c4aaf
Alex Dowad <alexinbeijing@gmail.com>
2015-08-29 22:52:07 +0200

    Use longs instead of ints

--- ext/popcount/popcount.c
+++ ext/popcount/popcount.c
@@ -3,8 +3,8 @@
 static VALUE
 fnum_popcount(VALUE fnum)
 {
-  int bits = __builtin_popcount(FIX2INT(fnum));
-  return INT2FIX(bits);
+  long bits = __builtin_popcountl(FIX2LONG(fnum));
+  return LONG2FIX(bits);
 }
 
 static VALUE
@@ -12,12 +12,14 @@ bnum_popcount(VALUE bnum)
 {
   BDIGIT *digits = RBIGNUM_DIGITS(bnum);
   size_t  length = RBIGNUM_LEN(bnum);
-  int     bits = 0;
+  long    bits = 0;
 
-  while (length--)
-    bits += __builtin_popcount(*digits++);
+  while (length--) {
+    bits += __builtin_popcountl((long)*digits);
+    digits++;
+  }
 
-  return INT2FIX(bits);
+  return LONG2FIX(bits);
 }
 
 void Init_popcount(void)
