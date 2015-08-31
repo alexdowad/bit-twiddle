cf12ba198072144d8c219556ddeeb3e22a946c89
Alex Dowad <alexinbeijing@gmail.com>
2015-08-31 10:56:26 +0200

    Add #lo_bit and #hi_bit

--- ext/popcount/popcount.c
+++ ext/popcount/popcount.c
@@ -40,8 +40,59 @@ bnum_popcount(VALUE bnum)
   return LONG2FIX(bits);
 }
 
+static VALUE
+fnum_lo_bit(VALUE fnum)
+{
+  return LONG2FIX(__builtin_ffsl(FIX2LONG(fnum)));
+}
+
+static VALUE
+bnum_lo_bit(VALUE bnum)
+{
+  BDIGIT *digit = RBIGNUM_DIGITS(bnum);
+  long    bits  = 0;
+
+  while (!*digit) {
+    digit++;
+    bits += (sizeof(BDIGIT) * 8);
+  }
+
+  bits += ffs_bdigit(*digit);
+  return LONG2FIX(bits);
+}
+
+static VALUE
+fnum_hi_bit(VALUE fnum)
+{
+  long bits;
+  if (fnum == fix_zero) return fix_zero;
+  bits = __builtin_clzl(FIX2LONG(fnum));
+  return LONG2FIX((sizeof(long) * 8) - bits);
+}
+
+static VALUE
+bnum_hi_bit(VALUE bnum)
+{
+  BDIGIT *digit = RBIGNUM_DIGITS(bnum) + (RBIGNUM_LEN(bnum)-1);
+  long    bits  = (sizeof(BDIGIT) * 8) * RBIGNUM_LEN(bnum);
+
+  while (!*digit) {
+    digit--;
+    bits -= (sizeof(BDIGIT) * 8);
+  }
+
+  bits -= clz_bdigit(*digit);
+  return LONG2FIX(bits);
+}
+
+
 void Init_popcount(void)
 {
   rb_define_method(rb_cFixnum, "popcount", fnum_popcount, 0);
   rb_define_method(rb_cBignum, "popcount", bnum_popcount, 0);
+
+  rb_define_method(rb_cFixnum, "lo_bit",   fnum_lo_bit, 0);
+  rb_define_method(rb_cBignum, "lo_bit",   bnum_lo_bit, 0);
+  rb_define_method(rb_cFixnum, "hi_bit",   fnum_hi_bit, 0);
+  rb_define_method(rb_cBignum, "hi_bit",   bnum_hi_bit, 0);
 }
