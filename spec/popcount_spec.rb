0442d001775e7f707720f184a3358d79be2ad98e
Alex Dowad <alexinbeijing@gmail.com>
2015-08-31 21:41:17 +0200

    Added specs for #popcount

---
+++ spec/popcount_spec.rb
@@ -0,0 +1,37 @@
+require 'popcount'
+
+describe "#popcount" do
+  it "returns 0 for 0" do
+    expect(0.popcount).to be 0
+  end
+
+  it "returns 1 for numbers with only a single 1 bit" do
+    0.upto(200) do |n|
+      expect((1 << n).popcount).to eq 1
+    end
+  end
+
+  it "returns 2 for numbers with 2 1 bits" do
+    0.upto(200) do |n|
+      expect((3 << n).popcount).to eq 2
+      expect((5 << n).popcount).to eq 2
+      expect((9 << n).popcount).to eq 2
+      unless n == 0
+        expect(((1 << n) + 1).popcount).to eq 2
+      end
+    end
+  end
+
+  it "returns N for (1 << N)-1" do
+    0.upto(200) do |n|
+      expect(((1 << n) - 1).popcount).to eq n
+    end
+  end
+
+  it "returns the same for negative numbers as for their absolute values" do
+    0.upto(100) do |n|
+      num = -2 ** n
+      expect(num.popcount).to eq num.abs.popcount
+    end
+  end
+end
