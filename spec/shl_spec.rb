ed67d46e388bf1a150493ba622d5e87912d5693b
Alex Dowad <alexinbeijing@gmail.com>
2015-09-19 15:08:47 +0200

    Add specs for #lshift16

--- spec/shl_spec.rb
+++ spec/shl_spec.rb
@@ -49,6 +49,56 @@ describe "#lshift8" do
   end
 end
 
+describe "#lshift16" do
+  it "shifts bits in a 16-bit number to the left (but cuts off high bits)" do
+    100.times do
+      num = rand(1 << 16)
+      1.upto(20) do |sdist|
+        expect(num.lshift16(sdist)).to eq ((num << sdist) & MASK_16)
+      end
+    end
+  end
+
+  it "does a right shift if shift distance is negative" do
+    100.times do
+      num = rand(1 << 16)
+      1.upto(20) do |sdist|
+        expect(num.lshift16(-sdist)).to eq (num >> sdist)
+      end
+    end
+  end
+
+  it "returns the receiver if shift distance is zero" do
+    100.times do
+      num = rand(1 << 32)
+      expect(num.lshift16(0)).to eq num
+    end
+  end
+
+  it "fills in the low end with zeros" do
+    100.times do
+      num = rand(1 << 32)
+      1.upto(20) do |sdist|
+        mask = sdist <= 16 ? (1 << sdist) - 1 : MASK_16
+        expect(num.lshift16(sdist) & mask).to eq 0
+      end
+    end
+  end
+
+  it "doesn't modify bits above the 32nd" do
+    100.times do
+      num = rand(1 << 64)
+      -64.upto(64) do |sdist|
+        expect(num.lshift16(sdist) & ~MASK_16).to eq (num & ~MASK_16)
+      end
+    end
+  end
+
+  it "zeroes out low 32 bits when shift distance is a Bignum" do
+    expect(100.lshift16(1 << 100)).to eq 0
+    expect((1 << 100).lshift16(1 << 100)).to eq (1 << 100)
+  end
+end
 
 describe "#lshift32" do
   it "shifts bits in a 32-bit number to the left (but cuts off high bits)" do
