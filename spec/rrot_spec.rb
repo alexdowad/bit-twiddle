fc976223538f24b3c9220149a7404ae97acd172c
Alex Dowad <alexinbeijing@gmail.com>
2015-09-21 05:41:06 +0200

    Unify rrot specs for various bit widths

--- spec/rrot_spec.rb
+++ spec/rrot_spec.rb
@@ -1,303 +1,79 @@
-describe "#rrot8" do
-  it "rotates the low 8 bits to the right" do
-    100.times do
-      num  = rand(1 << 8)
-      bnum = rand(1 << 90)
-      1.upto(7) do |rdist|
-        mask = ~((1 << rdist) - 1) & MASK_8
-        expect(num.rrot8(rdist)).to eq (((num & mask) >> rdist) | ((num & ~mask) << (8 - rdist)))
-        expect(bnum.rrot8(rdist)).to eq (((bnum & mask) >> rdist) | (((bnum & ~mask) & MASK_8) << (8 - rdist)) | (bnum & ~MASK_8))
-      end
-    end
-  end
+[[:rrot8, 8], [:rrot16, 16], [:rrot32, 32], [:rrot64, 64]].each do |method, bits|
+  describe "##{method}" do
+    bitmask = (1 << bits) - 1
 
-  it "doesn't modify the higher bits" do
-    100.times do
-      num  = rand(1 << 32)
-      bnum = rand(1 << 90)
-      1.upto(30) do |rdist|
-        expect(num.rrot8(rdist) & ~MASK_8).to eq (num & ~MASK_8)
-        expect(bnum.rrot8(rdist) & ~MASK_8).to eq (bnum & ~MASK_8)
-      end
-    end
-  end
-
-  context "with a negative rotate distance" do
-    it "rotates the low 8 bits to the left" do
-      100.times do
-        num  = rand(1 << 8)
-        bnum = rand(1 << 90)
-        1.upto(7) do |rdist|
-          mask = (1 << (8 - rdist)) - 1
-          expect(num.rrot8(-rdist)).to eq (((num & mask) << rdist) | ((num & ~mask) >> (8 - rdist)))
-          expect(bnum.rrot8(-rdist)).to eq (((bnum & mask) << rdist) | (((bnum & ~mask) & MASK_8) >> (8 - rdist)) | (bnum & ~MASK_8))
-        end
-      end
-    end
-  end
-
-  context "with a rotate distance greater than 7" do
-    it "does the same as (rotate distance) % 8" do
+    it "rotates the low #{bits} bits to the right" do
       100.times do
         num  = rand(1 << 32)
         bnum = rand(1 << 90)
-        8.upto(100) do |n|
-          expect(num.rrot8(n)).to eq num.rrot8(n % 8)
-          expect(bnum.rrot8(n)).to eq bnum.rrot8(n % 8)
+        1.upto(bits) do |rdist|
+          mask = ~((1 << rdist) - 1) & bitmask
+          expect(num.send(method, rdist)).to  eq (((num  & mask) >> rdist) | (((num  & ~mask) & bitmask) << (bits - rdist)) | (num  & ~bitmask))
+          expect(bnum.send(method, rdist)).to eq (((bnum & mask) >> rdist) | (((bnum & ~mask) & bitmask) << (bits - rdist)) | (bnum & ~bitmask))
         end
       end
     end
-  end
 
-  context "with a Bignum as rotate distance" do
-    it "still works the same" do
-      100.times do
-        num   = rand(1 << 32)
-        bnum  = rand(1 << 90)
-        dist = rand(1 << 90)
-        0.upto(20) do |offset|
-          rdist = dist + offset
-          expect(num.rrot8(rdist)).to eq num.rrot8(rdist % 8)
-          expect(bnum.rrot8(rdist)).to eq bnum.rrot8(rdist % 8)
-          expect(num.rrot8(rdist) & ~MASK_8).to eq (num & ~MASK_8)
-          expect(bnum.rrot8(rdist) & ~MASK_8).to eq (bnum & ~MASK_8)
-        end
-      end
-    end
-  end
-
-  context "with a 0 as rotate distance" do
-    it "returns the receiver" do
-      expect(100.rrot8(0)).to eq 100
-      expect((111 << 80).rrot8(0)).to eq (111 << 80)
-    end
-  end
-end
-
-describe "#rrot16" do
-  it "rotates the low 16 bits to the right" do
-    100.times do
-      num  = rand(1 << 16)
-      bnum = rand(1 << 90)
-      1.upto(15) do |rdist|
-        mask = ~((1 << rdist) - 1) & MASK_16
-        expect(num.rrot16(rdist)).to eq (((num & mask) >> rdist) | ((num & ~mask) << (16 - rdist)))
-        expect(bnum.rrot16(rdist)).to eq (((bnum & mask) >> rdist) | (((bnum & ~mask) & MASK_16) << (16 - rdist)) | (bnum & ~MASK_16))
-      end
-    end
-  end
-
-  it "doesn't modify the higher bits" do
-    100.times do
-      num  = rand(1 << 32)
-      bnum = rand(1 << 90)
-      1.upto(30) do |rdist|
-        expect(num.rrot16(rdist) & ~MASK_16).to eq (num & ~MASK_16)
-        expect(bnum.rrot16(rdist) & ~MASK_16).to eq (bnum & ~MASK_16)
-      end
-    end
-  end
-
-  context "with a negative rotate distance" do
-    it "rotates the low 16 bits to the left" do
-      100.times do
-        num  = rand(1 << 16)
-        bnum = rand(1 << 90)
-        1.upto(15) do |rdist|
-          mask = (1 << (16 - rdist)) - 1
-          expect(num.rrot16(-rdist)).to eq (((num & mask) << rdist) | ((num & ~mask) >> (16 - rdist)))
-          expect(bnum.rrot16(-rdist)).to eq (((bnum & mask) << rdist) | (((bnum & ~mask) & MASK_16) >> (16 - rdist)) | (bnum & ~MASK_16))
-        end
-      end
-    end
-  end
-
-  context "with a rotate distance greater than 15" do
-    it "does the same as (rotate distance) % 16" do
+    it "doesn't modify the higher bits" do
       100.times do
         num  = rand(1 << 32)
         bnum = rand(1 << 90)
-        16.upto(100) do |n|
-          expect(num.rrot16(n)).to eq num.rrot16(n % 16)
-          expect(bnum.rrot16(n)).to eq bnum.rrot16(n % 16)
+        -(bits+10).upto(bits+10) do |rdist|
+          expect(num.send(method, rdist)  & ~bitmask).to eq (num  & ~bitmask)
+          expect(bnum.send(method, rdist) & ~bitmask).to eq (bnum & ~bitmask)
         end
       end
     end
-  end
 
-  context "with a Bignum as rotate distance" do
-    it "still works the same" do
-      100.times do
-        num   = rand(1 << 32)
-        bnum  = rand(1 << 90)
-        dist = rand(1 << 90)
-        0.upto(20) do |offset|
-          rdist = dist + offset
-          expect(num.rrot16(rdist)).to eq num.rrot16(rdist % 16)
-          expect(bnum.rrot16(rdist)).to eq bnum.rrot16(rdist % 16)
-          expect(num.rrot16(rdist) & ~MASK_16).to eq (num & ~MASK_16)
-          expect(bnum.rrot16(rdist) & ~MASK_16).to eq (bnum & ~MASK_16)
+    context "with a negative rotate distance" do
+      it "rotates the low #{bits} bits to the left" do
+        100.times do
+          num  = rand(1 << 32)
+          bnum = rand(1 << 90)
+          1.upto(bits) do |rdist|
+            mask = (1 << (bits - rdist)) - 1
+            expect(num.send(method, -rdist)).to  eq (((num  & mask) << rdist) | (((num  & ~mask) & bitmask) >> (bits - rdist)) | (num  & ~bitmask))
+            expect(bnum.send(method, -rdist)).to eq (((bnum & mask) << rdist) | (((bnum & ~mask) & bitmask) >> (bits - rdist)) | (bnum & ~bitmask))
+          end
         end
       end
     end
-  end
 
-  context "with a 0 as rotate distance" do
-    it "returns the receiver" do
-      expect(100.rrot16(0)).to eq 100
-      expect((111 << 80).rrot16(0)).to eq (111 << 80)
-    end
-  end
-end
-
-describe "#rrot32" do
-  it "rotates the low 32 bits to the right" do
-    100.times do
-      num  = rand(1 << 32)
-      bnum = rand(1 << 90)
-      1.upto(31) do |rdist|
-        mask = ~((1 << rdist) - 1) & MASK_32
-        expect(num.rrot32(rdist)).to eq (((num & mask) >> rdist) | ((num & ~mask) << (32 - rdist)))
-        expect(bnum.rrot32(rdist)).to eq (((bnum & mask) >> rdist) | (((bnum & ~mask) & MASK_32) << (32 - rdist)) | (bnum & ~MASK_32))
-      end
-    end
-  end
-
-  it "doesn't modify the higher bits" do
-    100.times do
-      num  = rand(1 << 32)
-      bnum = rand(1 << 90)
-      1.upto(50) do |rdist|
-        expect(num.rrot32(rdist) & ~MASK_32).to eq (num & ~MASK_32)
-        expect(bnum.rrot32(rdist) & ~MASK_32).to eq (bnum & ~MASK_32)
-      end
-    end
-  end
-
-  context "with a negative rotate distance" do
-    it "rotates the low 32 bits to the left" do
-      100.times do
-        num  = rand(1 << 32)
-        bnum = rand(1 << 90)
-        1.upto(31) do |rdist|
-          mask = (1 << (32 - rdist)) - 1
-          expect(num.rrot32(-rdist)).to eq (((num & mask) << rdist) | ((num & ~mask) >> (32 - rdist)))
-          expect(bnum.rrot32(-rdist)).to eq (((bnum & mask) << rdist) | (((bnum & ~mask) & MASK_32) >> (32 - rdist)) | (bnum & ~MASK_32))
+    context "with a rotate distance greater than #{bits-1}" do
+      it "does the same as (rotate distance) % #{bits}" do
+        100.times do
+          num  = rand(1 << 32)
+          bnum = rand(1 << 90)
+          8.upto(100) do |n|
+            expect(num.send(method, n)).to  eq num.send(method, n % bits)
+            expect(bnum.send(method, n)).to eq bnum.send(method, n % bits)
+          end
         end
       end
     end
-  end
 
-  context "with a rotate distance greater than 31" do
-    it "does the same as (rotate distance) % 32" do
-      100.times do
-        num  = rand(1 << 32)
-        bnum = rand(1 << 90)
-        32.upto(100) do |n|
-          expect(num.rrot32(n)).to eq num.rrot32(n % 32)
-          expect(bnum.rrot32(n)).to eq bnum.rrot32(n % 32)
+    context "with a Bignum as rotate distance" do
+      it "still works the same" do
+        100.times do
+          num   = rand(1 << 32)
+          bnum  = rand(1 << 90)
+          dist = rand(1 << 90)
+          0.upto(20) do |offset|
+            rdist = dist + offset
+            expect(num.send(method, rdist)).to  eq num.send(method, rdist % bits)
+            expect(bnum.send(method, rdist)).to eq bnum.send(method, rdist % bits)
+            expect(num.send(method, rdist)  & ~bitmask).to eq (num  & ~bitmask)
+            expect(bnum.send(method, rdist) & ~bitmask).to eq (bnum & ~bitmask)
+          end
         end
       end
     end
-  end
 
-  context "with a Bignum as rotate distance" do
-    it "still works the same" do
-      100.times do
-        num   = rand(1 << 32)
-        bnum  = rand(1 << 90)
-        dist = rand(1 << 90)
-        0.upto(20) do |offset|
-          rdist = dist + offset
-          expect(num.rrot32(rdist)).to eq num.rrot32(rdist % 32)
-          expect(bnum.rrot32(rdist)).to eq bnum.rrot32(rdist % 32)
-          expect(num.rrot32(rdist) & ~MASK_32).to eq (num & ~MASK_32)
-          expect(bnum.rrot32(rdist) & ~MASK_32).to eq (bnum & ~MASK_32)
-        end
+    context "with a 0 as rotate distance" do
+      it "returns the receiver" do
+        expect(100.send(method, 0)).to eq 100
+        expect((111 << 80).send(method, 0)).to eq (111 << 80)
       end
-    end
-  end
-
-  context "with a 0 as rotate distance" do
-    it "returns the receiver" do
-      expect(100.rrot32(0)).to eq 100
-      expect((111 << 80).rrot32(0)).to eq (111 << 80)
-    end
-  end
-end
-
-describe "#rrot64" do
-  it "rotates the low 64 bits to the right" do
-    100.times do
-      num  = rand(1 << 64)
-      bnum = rand(1 << 90)
-      1.upto(63) do |rdist|
-        mask = ~((1 << rdist) - 1) & MASK_64
-        expect(num.rrot64(rdist)).to eq (((num & mask) >> rdist) | ((num & ~mask) << (64 - rdist)))
-        expect(bnum.rrot64(rdist)).to eq (((bnum & mask) >> rdist) | (((bnum & ~mask) & MASK_64) << (64 - rdist)) | (bnum & ~MASK_64))
-      end
-    end
-  end
-
-  it "doesn't modify the higher bits" do
-    100.times do
-      num  = rand(1 << 32)
-      bnum = rand(1 << 90)
-      1.upto(80) do |rdist|
-        expect(num.rrot64(rdist) & ~MASK_64).to eq (num & ~MASK_64)
-        expect(bnum.rrot64(rdist) & ~MASK_64).to eq (bnum & ~MASK_64)
-      end
-    end
-  end
-
-  context "with a negative rotate distance" do
-    it "rotates the low 64 bits to the left" do
-      100.times do
-        num  = rand(1 << 64)
-        bnum = rand(1 << 90)
-        1.upto(63) do |rdist|
-          mask = (1 << (64 - rdist)) - 1
-          expect(num.rrot64(-rdist)).to eq (((num & mask) << rdist) | ((num & ~mask) >> (64 - rdist)))
-          expect(bnum.rrot64(-rdist)).to eq (((bnum & mask) << rdist) | (((bnum & ~mask) & MASK_64) >> (64 - rdist)) | (bnum & ~MASK_64))
-        end
-      end
-    end
-  end
-
-  context "with a rotate distance greater than 63" do
-    it "does the same as (rotate distance) % 64" do
-      100.times do
-        num  = rand(1 << 32)
-        bnum = rand(1 << 90)
-        64.upto(100) do |n|
-          expect(num.rrot64(n)).to eq num.rrot64(n % 64)
-          expect(bnum.rrot64(n)).to eq bnum.rrot64(n % 64)
-        end
-      end
-    end
-  end
-
-  context "with a Bignum as rotate distance" do
-    it "still works the same" do
-      100.times do
-        num   = rand(1 << 32)
-        bnum  = rand(1 << 90)
-        dist = rand(1 << 90)
-        0.upto(20) do |offset|
-          rdist = dist + offset
-          expect(num.rrot64(rdist)).to eq num.rrot64(rdist % 64)
-          expect(bnum.rrot64(rdist)).to eq bnum.rrot64(rdist % 64)
-          expect(num.rrot64(rdist) & ~MASK_64).to eq (num & ~MASK_64)
-          expect(bnum.rrot64(rdist) & ~MASK_64).to eq (bnum & ~MASK_64)
-        end
-      end
-    end
-  end
-
-  context "with a 0 as rotate distance" do
-    it "returns the receiver" do
-      expect(100.rrot64(0)).to eq 100
-      expect((111 << 80).rrot64(0)).to eq (111 << 80)
     end
   end
 end
